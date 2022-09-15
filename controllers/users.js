629ea4d0379851988f5f5f529f203ebe0dcd6e99
artem-liamichev <76532860+artem-liamichev@users.noreply.github.com>
2022-09-15 19:36:34 +0300

    project 13

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,13 @@ const createUser = (req, res) => {
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
-    .catch(() => { res.status(500).send({ message: 'Ошибка по умолчанию' }); });
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Ошибка по умолчанию' });
+      }
+    });
 };
 
 const getUserById = (req, res) => {
@@ -29,53 +35,63 @@ const getUserById = (req, res) => {
         res.status(200).send(user);
       }
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-      } else {
-        res.status(500).send({ message: 'Ошибка по умолчанию' });
-      }
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }))};
+
+// const getUsers = (req, res) => {
+//   try {
+//     const users = User.find({});
+//     res.status(200).send(users);
+//   } catch (e) {
+//     if (e.kind === 'ObjectId') {
+//       return res.status(400).send({ message: 'Переданы некорректные данные' });
+//     }
+//     res.status(500).send({message: 'Ошибка по умолчанию' });
+//   }
+// };
+
+// const getUserById = async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const user = await User.findById(id);
+//     if (!user) {
+//       return res
+//         .status(404)
+//         .send({ message: 'Пользователь не найден' });
+//     }
+//     res.status(200).send(user);
+//   } catch (e) {
+//     res.status(500).send({ message: 'Ошибка по умолчанию' });
+//   }
+// };
+
+const updateUserProfile = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, {
+      $set: { name: req.body.name, about: req.body.about },
     });
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: 'Пользователь не найден' });
+    }
+    res.status(200).send({ message: 'updated' });
+  } catch (e) {
+    res.status(500).send({ message: 'Ошибка по умолчанию' });
+  }
 };
 
-const updateUserProfile = (req, res) => {
-  User.findByIdAndUpdate(req.user._id, {
-    $set: { name: req.body.name, about: req.body.about },
-  }, { runValidators: true })
-    .then((user) => {
-      if (!user) {
-        res.status(404).send({ message: 'Пользователь не найден' });
-      } else {
-        res.status(200).send({ data: user });
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-      } else {
-        res.status(500).send({ message: 'Ошибка по умолчанию', err });
-      }
-    });
-};
-
-const updateUserAvatar = (req, res) => {
-  User.findByIdAndUpdate(req.user._id, {
-    $set: { avatar: req.body.avatar },
-  }, { runValidators: true })
-    .then((user) => {
-      if (!user) {
-        res.status(404).send({ message: 'Пользователь не найден' });
-      } else {
-        res.status(200).send({ data: user });
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-      } else {
-        res.status(500).send({ message: 'Ошибка по умолчанию' });
-      }
-    });
+const updateUserAvatar = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { $set: { avatar: req.body.avatar } });
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: 'Пользователь не найден' });
+    }
+    res.status(200).send({ message: 'updated' });
+  } catch (e) {
+    res.status(500).send({ message: 'Ошибка по умолчанию' });
+  }
 };
 
 module.exports = {
