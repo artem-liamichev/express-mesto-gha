d20aa65693d498c22e68ffa1d094fbfb2ef718b7
artem-liamichev <76532860+artem-liamichev@users.noreply.github.com>
2022-10-10 10:09:45 +0300

    project 14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedRequestError = require('./errors/UnauthorizedRequestError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedRequestError('Проблема с аутентификацией или авторизацией'));
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +15,9 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'SECRET');
   } catch (err) {
-    next(new UnauthorizedRequestError('Проблема с аутентификацией или авторизацией'));
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
 
   req.user = payload;
